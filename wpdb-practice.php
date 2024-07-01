9221c4d6ece7a8a1ce89417bc65a71c5494631b6
sbnoman01 <sbnoman27@gmail.com>
2024-07-01 16:21:08 +0600

    get post by term_id

--- wpdb-practice.php
+++ wpdb-practice.php
@@ -34,6 +34,10 @@ add_action('wp_body_open', function () {
     // get post category
     $sql = "SELECT wp_terms.name, wp_terms.slug, wp_terms.term_id FROM wp_term_taxonomy JOIN wp_terms ON wp_term_taxonomy.term_id = wp_terms.term_id WHERE wp_term_taxonomy.taxonomy = 'category'";
 
+    // get post by term id
+    $term_id = 5;
+    $sql = "SELECT * FROM wp_posts p RIGHT JOIN wp_term_relationships t ON p.id = t.object_id WHERE t.term_taxonomy_id = $term_id AND p.post_type = 'post' AND p.post_status = 'publish'";
+
     $res = $wpdb->get_results($sql);
 
     debug($res);
