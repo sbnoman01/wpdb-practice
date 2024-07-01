180f6ecd7647d4ac41b1aca81b94fd83dbb5a656
sbnoman01 <sbnoman27@gmail.com>
2024-07-01 16:07:35 +0600

    fatch post, join

--- wpdb-practice.php
+++ wpdb-practice.php
@@ -14,3 +14,28 @@ if (!defined('ABSPATH')) {
     die('Do not open this file directly.');
 }
 
+
+// function debug_($arr)
+// {
+//     echo '<pre>';
+
+//     print_r($arr);
+
+//     echo '</pre>';
+// }
+
+add_action('wp_body_open', function () {
+
+    global $wpdb;
+
+    // get_post
+    $sql = "SELECT post_title FROM `wp_posts` WHERE post_type = 'post' AND post_status = 'publish'";
+    
+    // get post category
+    $sql = "SELECT wp_terms.name, wp_terms.slug, wp_terms.term_id FROM wp_term_taxonomy JOIN wp_terms ON wp_term_taxonomy.term_id = wp_terms.term_id WHERE wp_term_taxonomy.taxonomy = 'category'";
+
+    $res = $wpdb->get_results($sql);
+
+    debug($res);
+
+});
