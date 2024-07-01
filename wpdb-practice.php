8dc26e326621deeffa601805e3c053d7ec43be49
sbnoman01 <sbnoman27@gmail.com>
2024-07-01 11:50:12 +0600

    init

---
+++ wpdb-practice.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  Plugin Name: WPDB Practice
+  Version: 1.0
+  Requires at least: 5.0
+  Requires PHP: 5.2
+  Tested up to: 6.5
+  Author: sbnoman01
+  Text Domain: wpdb-practice
+*/
+
+// include only file
+if (!defined('ABSPATH')) {
+    die('Do not open this file directly.');
+}
+
